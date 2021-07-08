59a21afe329f04394f1e7303a3e0e8c148665a36
id <id@IN0013477>
2021-07-08 16:17:43 +0900

    bj追加版

---
+++ src/main/java/com/example/demo/same/BlackJack.java
@@ -0,0 +1,34 @@
+package com.example.demo.same;
+
+public class BlackJack {
+
+	private int card;
+	private int pcard;
+
+	public BlackJack() {
+
+	}
+
+	public BlackJack(int card,int pcard) {
+		this.card = card;
+		this.pcard = pcard;
+	}
+
+	public int getCard() {
+		return card;
+	}
+
+	public void setCard(int card) {
+		this.card = card;
+	}
+
+	public int getPcard() {
+		return pcard;
+	}
+
+	public void setPcard(int pcard) {
+		this.pcard = pcard;
+	}
+
+
+}
