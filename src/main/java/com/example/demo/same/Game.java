a83246537b4d9986f5bf9a45a7fa52a9302cb7d2
id <id@IN0013383>
2021-07-08 11:41:08 +0900

    ゲームの中身以外殆ど出来ているんじゃない？

--- src/main/java/com/example/demo/same/Game.java
+++ src/main/java/com/example/demo/same/Game.java
@@ -2,6 +2,8 @@ package com.example.demo.same;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -9,6 +11,7 @@ import javax.persistence.Table;
 @Table(name="game")
 public class Game {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="code")
 	private Integer code;
 
